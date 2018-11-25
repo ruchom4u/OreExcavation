e1c46595e9d65d5407a51c2dad45e27a861e07c0
Funwayguy <FunwayguyEmail@gmail.com>
2018-11-25 20:05:33 +1000

    Updating repo from build 1.3.116 to 1.3.131

--- src/main/java/oreexcavation/undo/BlockHistory.java
+++ src/main/java/oreexcavation/undo/BlockHistory.java
@@ -6,6 +6,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.BlockSnapshot;
+import oreexcavation.core.OreExcavation;
 
 public class BlockHistory
 {
@@ -29,13 +30,27 @@ public class BlockHistory
 		
 		if(tile != null)
 		{
-			this.tileData = new NBTTagCompound();
-			tile.writeToNBT(tileData);
+            tileData = new NBTTagCompound();
+            
+            try
+            {
+                snapshot.getTileEntity().writeToNBT(tileData);
+            } catch(Exception e)
+            {
+                OreExcavation.logger.error("Unable to save undo state for tile entity: " + snapshot.getTileEntity().toString(), e);
+            }
 		} else
 		{
 			tileData = null;
 		}
 	}
+    
+    public BlockHistory(BlockSnapshot snapshot, NBTTagCompound tileNBT)
+    {
+        this.pos = snapshot.getPos();
+        this.state = snapshot.getReplacedBlock();
+        this.tileData = tileNBT;
+    }
 	
 	public void restoreBlock(World world)
 	{
